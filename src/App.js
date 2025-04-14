2f02dc481f57dae073e5488c313606d075dd451a
prieloizee <priscila.e.paulino@aluno.senai.br>
2025-04-14 16:18:57 -0300

    14/04

--- src/App.js
+++ src/App.js
@@ -1,34 +1,59 @@
-import React from 'react';
-import { NavigationContainer } from '@react-navigation/native';
-import { createStackNavigator } from '@react-navigation/stack';
-import Home from './screens/Home';
-import CadastroEvento from './screens/CadastroEvento';
-import CadastroOrganizador from './screens/CadastroOrganizador';
-import CadastroIngresso from './screens/CadastroIngresso';
+import React from "react";
+import { NavigationContainer } from "@react-navigation/native";
+import { createStackNavigator } from "@react-navigation/stack";
+import Home from "./screens/Home";
+import CadastroEvento from "./screens/CadastroEvento";
+import CadastroOrganizador from "./screens/CadastroOrganizador";
+import CadastroIngresso from "./screens/CadastroIngresso";
 import Login from "./screens/Login";
-import Cadastro from './screens/Cadastro';
-import Layout from "./components/Layout"
+import Cadastro from "./screens/Cadastro";
+import Layout from "./components/Layout";
+import TaskList from "./screens/TaskList";
+import TaskDetail from "./screens/TaskDetail";
+import EventosScreen from "./screens/EventosScreen";
 
 const Stack = createStackNavigator();
 
 export default function App() {
   return (
     <NavigationContainer>
-      <Stack.Navigator initialRouteName="Login">
-        <Stack.Screen name="Login" component={()=>(
-          <Layout>
-            <Login/>
+      <Stack.Navigator screenOptions={{headerShown:false}}>
+        {/* <Stack.Screen 
+        name="TaskList" 
+        component={TaskList}
+        />
+
+        <Stack.Screen 
+        name="TaskDetail" 
+        component={TaskDetail}
+        /> */}
+
+
+        <Stack.Screen
+          name="Login"
+          component={() => (
+            <Layout>
+              <Login />
             </Layout>
-        )} />
-        <Stack.Screen name="Cadastro" component={()=>(
-          <Layout>
-            <Cadastro/>
+          )}
+        />
+        <Stack.Screen
+          name="Cadastro"
+          component={() => (
+            <Layout>
+              <Cadastro />
             </Layout>
-        )} />
+          )}
+        />
         <Stack.Screen name="Home" component={Home} />
         <Stack.Screen name="CadastroEvento" component={CadastroEvento} />
-        <Stack.Screen name="CadastroOrganizador" component={CadastroOrganizador} />
+        <Stack.Screen name="CadastroOrganizador"component={CadastroOrganizador}/>
         <Stack.Screen name="CadastroIngresso" component={CadastroIngresso} />
+        <Stack.Screen name="EventosScreen">{()=>(
+          <Layout>
+            <EventosScreen/>
+          </Layout>
+        )}</Stack.Screen>
       </Stack.Navigator>
     </NavigationContainer>
   );
--- src/axios/axios.js
+++ src/axios/axios.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-    baseURL: "http://10.89.240.90:5000/api/v1/",
+    baseURL: "http://10.89.240.67:5000/api/v1/",
     headers:{
         'accept':'application/json'
     }
@@ -12,7 +12,8 @@ const sheets = {
     postUser:(user)=>api.post("user", user),
     postEvento:(evento)=>api.post("evento", evento),
     postOrganizador:(organizador)=>api.post("organizador", organizador),
-    postIngresso:(ingresso)=>api.post("ingresso", ingresso)
+    postIngresso:(ingresso)=>api.post("ingresso", ingresso),
+    getEventos: ()=> api.get("evento")
 }
 
 export default sheets;
